e8b5f6c4c507cf9058c734ee1af882332c3d0d40
djembaraa <djembararafat98@gmail.com>
2025-12-16 23:58:40 +0700

    feat(showcase): add new showcase sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Hero from './sections/Hero'
+import ShowcaseSections from './sections/ShowcaseSections'
 
 function App() {
   return (
     <>
     <Hero/>
+    <ShowcaseSections/>
     </>
   )
 }
